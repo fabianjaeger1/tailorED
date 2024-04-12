d6d2f2f79820ec428c5d5d409ec60285157b026f
Axel Barbelanne <axel.barbelanne@gmail.com>
2024-04-13 01:07:26 +0200

    init commit

---
+++ components/keypoints.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+import { Button, type ButtonProps } from '@/components/ui/button'
+import { IconGitHub, IconSpinner } from '@/components/ui/icons'
+
+
+
+export function KeyComponents({
+    return (
+        <div>Test</div>
+    )
+};
+
+export {
+    LoginButton
+}
--- flaskr/app.py
+++ flaskr/app.py
@@ -3,8 +3,13 @@ import pandas as pd
 from flask_cors import CORS
 import urllib.parse
 
+import openai
+import subprocess
+
 app = Flask(__name__)
 CORS(app)
+
+
 
 @app.route("/company-names")
 def company_names():
@@ -23,3 +28,41 @@ def company_esg(company):
         'social': float(s[s['companyLongName'] == parsedCompanyName]['socialFactorAverage'].iloc[0]),
         'governance': float(g[g['companyLongName'] == parsedCompanyName]['governanceFactorAverage'].iloc[0])
     }
+
+@app.route("/company-esg/<company>")
+def convert_audio(input_file, output_file):
+    """
+    Convert an audio file to Opus format using FFmpeg.
+    input_file: input audio file (mp3)
+    output_file: output audio file (.ogg)
+    """
+
+    # Define the FFmpeg command as a list of arguments
+    command = [
+        'ffmpeg',
+        '-i', input_file,   # Input file
+        '-vn',              # No video
+        '-map_metadata', '-1',  # Strip metadata
+        '-ac', '1',         # Set audio channels to 1 (mono)
+        '-c:a', 'libopus',  # Codec for audio: Opus
+        '-b:a', '12k',      # Bitrate for audio: 12 kbps
+        '-application', 'voip',  # Optimize for VoIP
+        output_file         # Output file
+    ]
+    
+    try:
+        subprocess.run(command, check=True)
+        print(f"File converted successfully: {output_file}")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while converting the file: {e}")
+
+
+@app.route("/company-esg/<company>")
+def transcribe_audio(audio_file):
+    
+    response = openai.Audio.transcribe(
+        model="whisper-1",
+        file=open(audio_file, "rb")
+    )
+    return response['text']
+
